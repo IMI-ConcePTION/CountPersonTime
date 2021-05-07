51114bd0bdb90ee3e485f92bc11b1a27e6ec6284
RosaMGini <rosa.gini@ars.toscana.it>
2021-05-06 18:06:25 -0700

    created test environment

---
+++ examples/01_example_regular_use/to_run.R
@@ -0,0 +1,53 @@
+#-------------------------------
+# example 1: regular use of the function 
+
+rm(list=ls(all.names=TRUE))
+
+#set the directory where the file is saved as the working directory
+if (!require("rstudioapi")) install.packages("rstudioapi")
+thisdir <- setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+thisdir <- setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+#load function
+source(paste0(thisdir,"/../../CountPersonTimeV9.R"))
+
+# load data.table
+if (!require("data.table")) install.packages("data.table")
+library(data.table)
+
+
+#load input
+Input_file1<-fread(paste0(thisdir,"/input/input_events.csv"), sep = ",")
+Input_file2<-fread(paste0(thisdir,"/input/input_times.csv"), sep = ",")
+
+date_cols<-c("start_date","end_date","date_birth")
+Input_file2[,(date_cols) := lapply(.SD,function(x) (as.character(x))),.SDcols=date_cols]
+Input_file2[,(date_cols) := lapply(.SD,function(x) (as.Date(x,"%Y%m%d"))),.SDcols=date_cols]
+Input_file1[,date_event := as.character(date_event)]
+Input_file1[,date_event:= as.Date(date_event,"%Y%m%d")]
+
+
+
+#Input_file2[2,4]<-NA
+
+
+Output_file1<-CountPersonTime(
+  Dataset_events = Input_file1, 
+  Dataset = Input_file2,
+  Person_id = "person_id",
+  Start_study_time = "20150101",
+  End_study_time = "20191231",
+  Start_date = "start_date", 
+  End_date = "end_date",
+  Birth_date = "date_birth",
+  Strata = c("sex","city"),
+  Name_event = "name_event",
+  Date_event = "date_event",
+  Age_bands = c(0,17,44,64),
+  Increment="year",
+  Outcomes =c("outcome1","outcome2"),
+  Unit_of_age = "year",
+  include_remaning_ages = T,
+  Aggregate = F
+)
+
