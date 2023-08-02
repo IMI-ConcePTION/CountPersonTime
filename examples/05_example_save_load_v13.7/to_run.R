bf007a0410c8746fcdcd97d9b8011b847bd6fd70
Davide <davide.messina@ars.toscana.it>
2023-08-02 11:17:55 +0200

    example 5

--- examples/05_example_save_load_v13.7/to_run.R
+++ examples/05_example_save_load_v13.7/to_run.R
@@ -43,8 +43,42 @@ standard <- CountPersonTime(
   Outcomes_nrec =c("outcome1","outcome2"),
   Unit_of_age = "year",
   include_remaning_ages = T,
+  Aggregate = F,
+  save_intermediate = file.path(thisdir, "intermediate"),
+  split_by = c("sex","city", 2)
+)
+
+source(paste0(thisdir,"/../../../CountPersonTime/CountPersonTimeV13.8.R"))
+
+standard1 <- CountPersonTime(
+  Dataset_events = Input_file1, 
+  Dataset = Input_file2,
+  Person_id = "person_id",
+  Start_study_time = "20120101",
+  End_study_time = "20191231",
+  Start_date = "start_date", 
+  End_date = "end_date",
+  Birth_date = "date_birth",
+  Strata = c("sex","city"),
+  Name_event = "name_event",
+  Date_event = "date_event",
+  Age_bands = c(0,17,44,64),
+  Increment="year",
+  Outcomes_nrec =c("outcome1","outcome2"),
+  Unit_of_age = "year",
+  include_remaning_ages = T,
   Aggregate = F
 )
+
+
+
+
+
+
+
+
+
+
 
 simple_save <- CountPersonTime(
   Dataset_events = Input_file1, 
